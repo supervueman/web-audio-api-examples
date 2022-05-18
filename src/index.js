3901479c436c00196e9eb039eb3f47fd8eff8ac5
supervueman <supervueman@gmail.com>
2022-05-18 23:00:02 +0400

    Add gain controller

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
   let context;
   let bufferLoader;
+  let gain;
 
   function init() {
     context = new AudioContext();
+    gain = context.createGain()
+    console.log(gain)
 
     bufferLoader = new BufferLoader(
       context,
@@ -18,6 +21,11 @@ window.addEventListener('load', () => {
 
   function finishedLoading(bufferList) {
     const source = context.createBufferSource();
+    source.connect(gain)
+    gain.connect(context.destination)
+    gain.gain.setValueAtTime(0, context.currentTime)
+
+    console.log(context.destination)
     source.buffer = bufferList[0];
 
     source.connect(context.destination);
@@ -26,5 +34,11 @@ window.addEventListener('load', () => {
 
   document.querySelector('.btn-init').addEventListener('click', () => {
     init()
+  })
+
+  document.querySelector('.input-gain').addEventListener('input', (e) => {
+    const gainValue = e.target.value / 10 - 1
+
+    gain.gain.setValueAtTime(gainValue, context.currentTime)
   })
 })
