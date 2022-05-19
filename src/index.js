0e53f60d409a5c97c1197660f5e3e54d13e08680
supervueman <supervueman@gmail.com>
2022-05-19 22:33:49 +0400

    Add delay effect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ window.addEventListener('load', () => {
   let context;
   let bufferLoader;
   let gain;
+  let delay;
 
   function init() {
     context = new AudioContext();
     gain = context.createGain()
+    delay = context.createDelay(1)
     console.log(gain)
 
     bufferLoader = new BufferLoader(
@@ -22,11 +24,14 @@ window.addEventListener('load', () => {
   function finishedLoading(bufferList) {
     const source = context.createBufferSource();
     source.connect(gain)
+    source.connect(delay)
+    delay.connect(context.destination)
     gain.connect(context.destination)
     gain.gain.setValueAtTime(0, context.currentTime)
 
     console.log(context.destination)
     source.buffer = bufferList[0];
+    console.log(bufferList)
 
     source.connect(context.destination);
     source.start(0);
@@ -36,9 +41,15 @@ window.addEventListener('load', () => {
     init()
   })
 
-  document.querySelector('.input-gain').addEventListener('input', (e) => {
-    const gainValue = e.target.value / 10 - 1
+  document.querySelector('.effect--control-gain').addEventListener('input', (e) => {
+    const value = e.target.value / 10 - 1
+    gain.gain.setValueAtTime(value, context.currentTime)
+    document.querySelector('.effect--gain').querySelector('.effect--value').innerText = value
+  })
 
-    gain.gain.setValueAtTime(gainValue, context.currentTime)
+  document.querySelector('.effect--control-delay').addEventListener('input', (e) => {
+    const value = e.target.value / 100
+    delay.delayTime.setValueAtTime(value, context.currentTime)
+    document.querySelector('.effect--delay').querySelector('.effect--value').innerText = value
   })
 })
